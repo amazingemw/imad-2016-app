9d756ecbb4cde16b1b93501248afea86956a340a
amazingemw <amazingemw@users.noreply.github.com>
2016-09-30 20:25:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,50 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var pageone ={
+    title: "Page 1 | Emwhack",
+    heading: "Page 1",
+    date: "September 30,2016",
+    content:`<p><i>The day turned to night, yet my dog never learned to bite. Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment.</i></p>
+            <div>
+                <p><b>Ever been to the moon, but had to return because there was no saloon? Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment.</b></p>
+            </div>
+            <div>
+                <p><i>Plastic unicorns made my day, burping rainbows all the way Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment. Filler Text is a big embarassment to the establishment.</i></p>`
+}
+
+function createTemplate (data){
+    var title = data.title;
+    var date = data.date
+    var heading = data.heading;
+    var content = data.contenet;
+}
+
+var htmltemplate = `
+<html>
+    <head>
+        ${title}
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href='/'>Home</a>
+            </div>
+            <hr/>
+            <div>
+                ${date}
+            </div>
+            ${content}
+        </div>
+    </body>
+</html> 
+`;
+
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(pageone));
 });
 
 app.get('/page1',function(req, res){
