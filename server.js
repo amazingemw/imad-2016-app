5c87f79483e4a929f9eec1dfa4829cba32c52d34
amazingemw <amazingemw@users.noreply.github.com>
2016-09-30 19:26:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/page1',function(req, res){
 });
 
 app.get('/page2',function(req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+     res.sendFile(path.join(__dirname, 'ui', 'page2.html'));
 });
 
 app.get('/page3',function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'page3.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
