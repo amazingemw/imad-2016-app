bbb9e4356d164db3b19aecefad76d1813003083f
amazingemw <amazingemw@users.noreply.github.com>
2016-09-30 14:29:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/page1',function(req, res){
+    res.send("The page one of my webpage is accesible via net");
+});
+
+app.get('/page1',function(req, res){
+    res.send("The page one of my webpage is accesible via net");
+});
+
+app.get('/page1',function(req, res){
+    res.send("The page one of my webpage is accesible via net");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
