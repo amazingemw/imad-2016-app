eb1b21ea870d9f1b2a3b8214c131f842ad2525dc
amazingemw <amazingemw@users.noreply.github.com>
2016-10-13 21:26:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,17 +21,33 @@ button.onclick = function(){
 };
     
 //Submit name
-/*var nameInput = document.getElementById("name");
+var nameInput = document.getElementById("name");
 var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function (){
     //MAke request to server and send the name
-    //Capture a list of names and render it as a list
-    var names= ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list += '<li> ' + names[i] + '</li>';
+     
+     var request = new XMLHttpRequest();
+    //Capture response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                 //Capture a list of names and render it as a list
+                var names= request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                    list += '<li> ' + names[i] + '</li>';
+                    }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
         }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-};*/
+        //Not Do  ne yet
+    };
+    //Make the request
+    request.open('GET', 'http://amazingemw.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
+    
+   
+};
