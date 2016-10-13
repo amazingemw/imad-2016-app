f42e67eb163c05a9d7cec5c32fd1c3d0fc48789a
amazingemw <amazingemw@users.noreply.github.com>
2016-10-13 19:00:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ function movRight(){
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
-    var interval = setInterval(movRight,100);
+    var interval = setInterval(movRight,1000);
 };
     
