935178fb33c944668c1875cf7690f4dff2c834f2
amazingemw <amazingemw@users.noreply.github.com>
2016-10-13 21:03:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ button.onclick = function(){
 };
     
 //Submit name
-var nameInput = document.getElementById("name");
+/*var nameInput = document.getElementById("name");
 var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function (){
@@ -34,4 +34,4 @@ submit.onclick = function (){
         }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
-};
+};*/
